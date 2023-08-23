c978c970a360a5270d9668b33a8f9ba84d011f65
ankushrwt602 <142775190+ankushrwt602@users.noreply.github.com>
2023-08-23 13:06:02 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainComponent from "./Component/MainComponent";
 import LoginComponent from "./Component/LoginComponent";
 import ApplyleaveComponent from "./Component/ApplyleaveComponent";
+import DashboardComponent from "./Component/DashboardComponent";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/main" element={<MainComponent />} />
       <Route path="/" element={<LoginComponent />} />
       <Route path="/main/apply-leave" element={<ApplyleaveComponent/>} />
+      <Route path="/main/dashboard-employee" element={<DashboardComponent/>}/>
     </Routes>
   </Router>
   </>
